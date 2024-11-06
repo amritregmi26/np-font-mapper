e854bca7dc0484eb69b68a78a3872ca5b54a44d4
Amrit <amrregmi@gmail.com>
2024-11-06 07:43:25 +0545

    feat: implement font mapping logic
    1. Prepared mapping json file for 4 different fonts
    2. implemented font mapper class

--- src/font-mapper-np/language_detection.py
+++ src/font-mapper-np/language_detection.py
@@ -3,10 +3,11 @@ import torch
 import torch.nn.functional as F
 import Levenshtein
 from sentence_transformers import SentenceTransformer
+from typing import List
 
 
 class DetectLanguage:
-    def __init__(self):
+    def __init__(self) -> None:
         try:
             english_words = nltk.corpus.words.words()
         except LookupError as e:
@@ -16,7 +17,7 @@ class DetectLanguage:
         self.embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
         self.similarity_threshold = 0.70
 
-    def similar_words(self, query, k=10):
+    def _similar_words(self, query: str , k = 10) -> List[str]:
         distances = {}
         for word in self.words:
             distances[word] = Levenshtein.distance(query.lower(), word)
@@ -24,8 +25,8 @@ class DetectLanguage:
         similar_words = [word for word, _ in similar_words[:k]]
         return similar_words
     
-    def get_cosine_similarity(self, query):
-        similar_levenshtein_words = self.similar_words(query)
+    def _get_cosine_similarity(self, query: str) -> List[float]:
+        similar_levenshtein_words = self._similar_words(query)
         embeddings = self.embedding_model.encode(similar_levenshtein_words)
         input_vector = torch.tensor(self.embedding_model.encode(query.lower()))
         cosine_similarities = []
@@ -34,11 +35,11 @@ class DetectLanguage:
             cosine_similarities.append(F.cosine_similarity(embd, input_vector, dim=0).item())
         return cosine_similarities
     
-    def detect_language(self, query):
-        cosine_similarities = self.get_cosine_similarity(query)
+    def detect_language(self, query: str) -> str:
+        cosine_similarities = self._get_cosine_similarity(query)
         max_similarity = max(cosine_similarities)
         if max_similarity >= self.similarity_threshold:
-            return "english"
+            return "en-US"
         else:
-            return "nepali"
+            return "ne-NP"
         
