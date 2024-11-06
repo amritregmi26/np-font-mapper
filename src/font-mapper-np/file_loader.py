4d5e92a5fc42c28537e8b33b0670e6a7f0a28337
Amrit <amrregmi@gmail.com>
2024-11-06 19:46:13 +0545

    feat: preserve unicode nepali
    1. add feature to get all available fonts
    2. Avoid mapping for unicode based nepali characters

--- src/font-mapper-np/file_loader.py
+++ src/font-mapper-np/file_loader.py
@@ -1,15 +1,10 @@
 import pymupdf
-import mimetypes
 
 class FileHanlder:
     def __init__(self, file_path):
         self.file_path = file_path
         
     def load_and_extract(self):
-        # file_type = mimetypes.guess_type(self.file_path)
         with pymupdf.open(self.file_path) as doc:
             content = "".join([page.get_text() for page in doc])
         return content
-    
-    # def pdf_to_txt(self):
-    #     pass
